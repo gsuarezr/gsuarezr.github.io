097829638cbda48c89ac34b6eb332647e68c4afc
mcditooss <gerardofingurbe@gmail.com>
2025-07-14 17:01:40 +0200

    actions failing

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_ZytPOb from "polynomial_roots.gif?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fjax_root_pol.md";
+export default new Map([["polynomial_roots.gif?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fjax_root_pol.md", __ASTRO_IMAGE_IMPORT_ZytPOb]]);
+		
